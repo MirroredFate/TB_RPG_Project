cd7b4bc04411556a391136e01184cf83be4534cd
MirroredFate <Karl_daniel@hotmail.de>
2021-08-23 22:38:23 +0200

    PlayerStats.cs added. Functionality follows

---
+++ PlayerStats.cs
@@ -0,0 +1,41 @@
+﻿namespace TB_RPG_Project
+{
+    public class PlayerStats
+    {
+        public int Strength
+        {
+            get => _strength;
+            set => _strength = value;
+        }
+
+        public int Stamina
+        {
+            get => _stamina;
+            set => _stamina = value;
+        }
+
+        public int Wisdom
+        {
+            get => _wisdom;
+            set => _wisdom = value;
+        }
+
+        public int Dexterity
+        {
+            get => _dexterity;
+            set => _dexterity = value;
+        }
+
+        public int Intelligence
+        {
+            get => _intelligence;
+            set => _intelligence = value;
+        }
+
+        private int _strength;
+        private int _stamina;
+        private int _wisdom;
+        private int _dexterity;
+        private int _intelligence;
+    }
+}
